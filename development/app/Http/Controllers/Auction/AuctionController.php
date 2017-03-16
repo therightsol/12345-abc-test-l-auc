a10abeddec8606753f071c8f82ae6b3105d552e7
fasihh <its.skp8@gmail.com>
2017-03-16 23:33:00 +0500

    auction search partial

--- development/app/Http/Controllers/Auction/AuctionController.php
+++ development/app/Http/Controllers/Auction/AuctionController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Auction;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Modules\Auctions\Entities\Auction;
 
 class AuctionController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, Filters $filter)
     {
+
+        $auctions = Auction::filter($filter)->get();
+
+        return $auctions;
+        $query = Auction::query();
+
+        if($request->has('max-year')){
+
+        }
+
+        dd($request->all());
 
         return view('auction.index');
         return $request->all();
