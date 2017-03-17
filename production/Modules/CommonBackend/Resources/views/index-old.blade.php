3dacb24a046b6e4f4b64c09d9471096629034916
Ali Shan <alishan@therightsol.com>
2017-03-17 23:41:41 +0500

    Production Removed
    Production folder removed

--- production/Modules/CommonBackend/Resources/views/index-old.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('commonbackend::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('commonbackend.name') !!}
-    </p>
-@stop
