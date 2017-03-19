f5aaa6c6b6e3f411cf43b3050b17207060642ca7
Ali Shan <alishan@therightsol.com>
2017-03-20 01:46:05 +0500

    Automatic Seed Command is Completed
    I created a custom command that will help to migrate + seed all things
    automatically.
    
    Command:
    php artisan automaticseed
    
    Moreover, some MySQL Constraint problems has been resolved for
    car_engines, car_features.

--- development/Modules/Cars/Database/Seeders/CarsDatabaseSeeder.php
+++ development/Modules/Cars/Database/Seeders/CarsDatabaseSeeder.php
@@ -26,13 +26,14 @@ class CarsDatabaseSeeder extends Seeder
         Model::unguard();
 
         $cat_size = Category::all();
+
         if (sizeof($cat_size) == 0){
             TestDummy::times(5)->create(Category::class);
         }
 
+        TestDummy::times(100)->create(Car::class);
         TestDummy::times(200)->create(CarCategories::class);
         TestDummy::times(100)->create(CarFeature::class);
-        TestDummy::times(100)->create(Car::class);
 
 
         // $this->call("OthersTableSeeder");
--- development/app/Console/Commands/AutoMaticSeed.php
+++ development/app/Console/Commands/AutoMaticSeed.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use DB;
 use Illuminate\Console\Command;
+use Schema;
 
 class AutoMaticSeed extends Command
 {
@@ -38,7 +40,60 @@ class AutoMaticSeed extends Command
     public function handle()
     {
         //
-        //$this->call('php artisan help migrate');
+        //$this->call('make:controller', ['name' => 'abc']);
 
+        // 1) getting database
+        $database_name = getenv('DB_DATABASE');
+        if (! $database_name || empty( $database_name ) ){
+            $this->error('Database name not resolved.');
+            $database_name = $this->ask("Please enter database name ");
+        }
+
+
+        // 2) Dropping tables (If already available)
+        $tables = DB::select('SHOW TABLES');
+        if (sizeof($tables) > 0){
+            $this->error("Database is already migrated.\n");
+            $this->info("Trying to re-migrate. Please wait ... \n");
+
+            $this->info("\nStep 1: Dropping Tables...\n");
+            do{
+                $confirm = $this->ask("Are you sure to delete all tables ? [yes / no]");
+                if (strtolower($confirm) === 'yes') break;
+            }while(strtolower($confirm) !== 'no');
+
+            $dbTable = 'Tables_in_' . $database_name;
+
+            $tables = DB::select('SHOW TABLES');
+            DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+            foreach($tables as $table)
+            {
+                Schema::drop($table->$dbTable);
+                echo 'Table ' . $table->$dbTable . " has been dropped\n";
+            }
+        }
+
+        // 2) Migrating
+        $this->info("\nStep 2: Migration Started...\n");
+        $this->call('migrate');
+        $this->info("\nMigration has been completed.");
+
+
+        // 3) Seeding
+        $this->info("\nStep 3: Seeding Started...\n");
+        $this->call('module:seed', ['module' => 'Users'] );
+        $this->call('module:seed', ['module' => 'Notifications'] );
+        $this->call('module:seed', ['module' => 'Media'] );
+        $this->call('module:seed', ['module' => 'GeneralSettings'] );
+        $this->call('module:seed', ['module' => 'Features'] );
+        $this->call('module:seed', ['module' => 'EngineTypes'] );
+        $this->call('module:seed', ['module' => 'CarCompanies'] );
+        $this->call('module:seed', ['module' => 'CarModels'] );
+        $this->call('module:seed', ['module' => 'Cars'] );
+        $this->call('module:seed', ['module' => 'InspectionRequests'] );
+        $this->call('module:seed', ['module' => 'Auctions'] );
+        $this->call('module:seed', ['module' => 'Biddings'] );
+        $this->call('module:seed', ['module' => 'Invoices'] );
     }
 }
